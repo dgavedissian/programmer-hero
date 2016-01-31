f4f06e249b4116dc8bde8228e654344f5c112264
David Avedissian <git@davedissian.com>
2016-01-31 01:07:41 +0000

    Moved projection calculation out of mainLoop

--- Main.hs
+++ Main.hs
@@ -23,8 +23,11 @@ data Renderables = Renderables {
 
 main :: IO ()
 main = do
+    let width = 1024
+        height = 768
+
     -- Create the window and store the window upate function
-    updateWindow <- initGL "Programmer Hero" 1024 768
+    updateWindow <- initGL "Programmer Hero" width height
 
     -- Set up rendering settings
     GL.clearColor $= GL.Color4 0.1 0.1 0.6 1
@@ -35,8 +38,12 @@ main = do
     -- Build scene and store entity render functions
     renderables <- Renderables <$> buildBoard <*> buildNote
 
+    -- Calculate the projection matrix
+    let aspect = (fromIntegral width) / (fromIntegral height)
+    let projMatrix = Camera.projectionMatrix (Camera.deg2rad 30) aspect 0.1 100
+
     -- Kick off the main loop
-    mainLoop camera updateWindow renderables
+    mainLoop camera updateWindow renderables projMatrix
     where
         -- Render Function
         render :: Renderables -> RenderContext -> IO ()
@@ -44,16 +51,14 @@ main = do
             renderBoard renderables cxt
 
         -- Main Loop
-        mainLoop :: Camera.Camera GLfloat -> IO InputState -> Renderables -> IO ()
-        mainLoop c updateWindow renderables = do
+        mainLoop :: Camera.Camera GLfloat -> IO InputState -> Renderables -> M44 GLfloat -> IO ()
+        mainLoop c updateWindow renderables projMatrix = do
             windowState <- updateWindow
 
             -- Clear framebuffer
             GL.clear [GL.ColorBuffer, GL.DepthBuffer]
 
             -- Calculate view and projection matrices
-            let V2 ww wh = fromIntegral <$> (windowSize windowState)
-            let projMatrix = Camera.projectionMatrix (Camera.deg2rad 30) (ww / wh) 0.01 100
             let viewMatrix = Camera.camMatrix c
 
             -- Draw using render parameters
@@ -63,6 +68,6 @@ main = do
             -- Quit if escaped has been pressed
             if S.member Key'Escape (keysPressed windowState)
             then return () -- terminate
-            else mainLoop c updateWindow renderables
+            else mainLoop c updateWindow renderables projMatrix
         camera = Camera.tilt (-20) $ Camera.dolly (V3 0 2 8) Camera.fpsCamera
 
